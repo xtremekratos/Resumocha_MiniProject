b23ac326d0ceb0c92c03c269b20ec44e5ef5a29e
jehadmohamedz <jehadmohamedz@gmail.com>
2018-11-20 19:50:07 +0530

    added ui for deletingaccount

--- user_info.php
+++ user_info.php
@@ -52,6 +52,14 @@ if($stmt = $mysqli->prepare($sql)){
         }
     }
 }
+
+//to add the code here foryes button of the form
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    echo "you del";
+}
+
+
+
 if(isset($_FILES['image'])){
     $errors= array();
     $file_name = $_FILES['image']['name'];
@@ -205,9 +213,9 @@ if(isset($_FILES['image'])){
                 <div class="col-md-12">
                     <div class="page-header clearfix">
                         <h2 class="pull-left">Your Details</h2>
-                        <button type="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#myModalResume"> <?php echo $resume_mode ?></button>
+                        <button type="button" class="btn btn-danger pull-right" data-toggle="modal" data-target="#myModalDelete"> <?php echo "Delete Account" ?></button>
+                        <button type="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#myModalResume" style="margin-right: 15px;"> <?php echo $resume_mode ?></button>
                         <button type="button" class="btn btn-success pull-right" data-toggle="modal" data-target="#myModalPic" style="margin-right: 15px;"> <?php echo $dp_mode ?></button>
-
                     <div class="modal fade" id="myModalResume" role="dialog">
                                 <div class="modal-dialog modal-lg">
                                 <div class="modal-content">
@@ -222,6 +230,22 @@ if(isset($_FILES['image'])){
                                     </div>
                                     <div class="modal-footer">
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+
+                             <div class="modal fade" id="myModalDelete" role="dialog">
+                                <div class="modal-dialog modal-sm">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Are you really sure you want to delete your account?</h4>
+                                    </div>
+                                    <div class="modal-body" align="center" >
+                                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                                    <input type = "submit" class="btn btn-danger btn-lg" style="margin-right: 15px;" value="Yes"/>
+                                    <button type="button" class="btn btn-warning btn-lg" data-dismiss="modal">No</button></form>
                                     </div>
                                 </div>
                                 </div>
