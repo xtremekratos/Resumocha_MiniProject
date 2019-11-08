9302a5ba6414db4f2f71b1a21fbdd89d6a057e05
ekokratos <adithyams.adi@gmail.com>
2019-11-08 09:14:03 +0530

    added tables for resume generator

--- predefined.sql
+++ predefined.sql
@@ -44,3 +44,52 @@ CREATE PROCEDURE DeleteAll(IN input_email VARCHAR(20),IN input_uid integer)
  DELETE from login_data where email=input_email;
  END //
 DELIMITER ;
+
+
+
+/*Resume generator*/
+
+CREATE TABLE login_data(email varchar(30) primary key,
+                        pass varchar(20) not null);
+
+CREATE TABLE user_info(
+    uid integer primary key AUTO_INCREMENT,
+    email varchar(30) unique references login_data(email),
+    name varchar(30) not null,
+    phone varchar(20),
+    byline varchar(40),
+    skills VARCHAR(40),
+    languages VARCHAR(40),
+    10_name VARCHAR(30),
+    10_marks VARCHAR(5),
+    10_year  CHAR(4),
+    12_name VARCHAR(30),
+    12_marks VARCHAR(5),
+    12_year  CHAR(4),
+    colg_name VARCHAR(30),
+    colg_marks VARCHAR(5),
+    colg_year  CHAR(4)
+);
+
+CREATE TABLE experience(
+    eid integer primary key AUTO_INCREMENT,
+    uid integer references user_info(uid),
+    name VARCHAR(30),
+    role VARCHAR(30),
+    desc VARCHAR(200),
+    year CHAR(4)
+);
+
+CREATE TABLE project(
+    pid integer primary key AUTO_INCREMENT,
+    uid integer references user_info(uid),
+    title VARCHAR(30),
+    desc VARCHAR(200)
+);
+
+CREATE TABLE achievement(
+    pid integer primary key AUTO_INCREMENT,
+    uid integer references user_info(uid),
+    title VARCHAR(30),
+    desc VARCHAR(200)
+);
